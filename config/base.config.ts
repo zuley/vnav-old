a28e61c7035d5d2c2768875f4bca3f20d3bfc113
zuley <914230482@qq.com>
2022-07-21 23:54:01 +0800

    更新配置

--- config/base.config.ts
+++ config/base.config.ts
@@ -1,14 +1,3 @@
-interface BaseConfig {
-  /** CMS 地址 */
-  cmsURL: string
-}
-
-const devConfig: BaseConfig = {
+export default {
   cmsURL: 'http://101.33.224.203:8056'
 }
-
-const prodConfig: BaseConfig = {
-  cmsURL: 'http://localhost:8056'
-}
-
-export default process.env.NODE_ENV === 'development' ? devConfig : prodConfig
